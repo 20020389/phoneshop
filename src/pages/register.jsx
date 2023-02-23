8113eff38aa83be2ef511987e9b81528581f039c
Nguyễn Quốc Đại <20020389@vnu.edu.vn>
2023-02-23 14:26:23 +0700

    test bot

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,24 +1,15 @@
-import { Box } from "@chakra-ui/react";
-import End from "../components/end";
-import Header1 from "../components/header1";
-import Registerr from "../components/Registerr";
- export default function Register(){
-    return (
-        <Box 
-     width='100vw'
-     height='100vh'
-    >
-        <Box 
-         width={'95%'}
-         height='100%'
-         position={'relative'}
-         left={'2.5%'}
-        >
-            <Header1></Header1> 
-            <Registerr namee="Đăng kí"></Registerr>
-            <End></End>  
-        </Box>
-         
+import { Box } from '@chakra-ui/react';
+import End from '../components/end';
+import Header1 from '../components/header1';
+import Registerr from '../components/Registerr';
+export default function Register() {
+  return (
+    <Box height="100vh">
+      <Box width={'100%'} height="100%" position={'relative'} left={'2.5%'}>
+        <Header1></Header1>
+        <Registerr namee="Đăng kí"></Registerr>
+        <End></End>
+      </Box>
     </Box>
-    )
- }
+  );
+}
