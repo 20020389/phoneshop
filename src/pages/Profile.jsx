60c5871d63f5f1c6bada05c1ddeb2e5eb7cdcb57
Nguyễn Quốc Đại <20020389@vnu.edu.vn>
2023-03-16 07:18:23 +0700

    update profile render

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,21 +1,22 @@
 import { Fragment, useMemo } from 'react';
 import { useStore } from '../lib/zustand';
-import { StoreProfile } from '../components/Profile';
+import { Profile } from '../components/Profile';
 import Header1 from '../components/header1';
 
-export function Profile() {
+export function ProfilePage() {
   const user = useStore((store) => store.user);
 
   const renderProfile = useMemo(() => {
     if (user.role === 'STORE') {
-      return <StoreProfile user={user} />;
+      return <Fragment></Fragment>;
     }
     return <Fragment></Fragment>;
   }, [user]);
 
   return (
     <div>
-      <Header1></Header1>
+      <Header1 />
+      <Profile user={user} />
       {renderProfile}
     </div>
   );
