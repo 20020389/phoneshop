29721fe9c6c55a1e43200ea9287cb34f45475303
Nguyễn Quốc Đại <20020389@vnu.edu.vn>
2023-04-08 23:50:22 +0700

    update header

--- src/components/header2.jsx
+++ src/components/header2.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import '../CSS/header2.css'
-export default function Header2(){
-    return(
-            <div className='header3'>
-                <div id='Left'></div>
-                <div id='Right'>
-                    <div id='top'></div>
-                    <div id='mid'></div>
-                    <div id='bot'></div>
-                </div>
-            </div>
-        
-    )
+import React, { memo } from 'react';
+import '../CSS/header2.css';
+function Header2() {
+  return (
+    <div className="shadow-xl rounded-[10px]">
+      <div className="header3 !mt-5 rounded-[10px] overflow-hidden">
+        <div id="Left"></div>
+        <div id="Right">
+          <div id="top"></div>
+          <div id="bot"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
+export default memo(Header2);
