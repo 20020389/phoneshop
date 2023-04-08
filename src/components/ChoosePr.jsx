74d25d4f7716463013d9ea22207aed1ffa2817ac
Nguyễn Quốc Đại <20020389@vnu.edu.vn>
2023-04-08 23:52:11 +0700

    change trademark icon

--- src/components/ChoosePr.jsx
+++ src/components/ChoosePr.jsx
@@ -1,31 +1,66 @@
 import { Box } from '@chakra-ui/react';
-import But from './button';
-export default function ChoosePr() {
+import { memo } from 'react';
+import { Link } from 'react-router-dom';
+
+const items = [
+  {
+    src: '/trademark/iPhone-1.png',
+    to: '/search?trademark=iphone',
+  },
+  {
+    src: '/trademark/Appo.png',
+    to: '/search?trademark=appo',
+  },
+  {
+    src: '/trademark/Huawei-1.png',
+    to: '/search?trademark=huawei',
+  },
+  {
+    src: '/trademark/logo_realme.jpg',
+    to: '/search?trademark=realme',
+  },
+  {
+    src: '/trademark/oneplus.png',
+    to: '/search?trademark=oneplus',
+  },
+  {
+    src: '/trademark/Samsung-1.png',
+    to: '/search?trademark=samsung',
+  },
+  {
+    src: '/trademark/vivologo_akm4.jpg',
+    to: '/search?trademark=vivo',
+  },
+  {
+    src: '/trademark/xiaomi.png',
+    to: '/search?trademark=xiaomi',
+  },
+];
+
+function ChoosePr() {
   return (
-    <Box width={'100%'} height="100px">
-      <Box
-        width="100%"
-        height={'52px'}
-        marginTop="20px"
-        display="flex"
-        justifyContent="space-between"
-      >
-        <But Name="Apple"></But>
-        <But Name="Samsung"></But>
-        <But Name="Samsung"></But>
-        <But Name="Samsung"></But>
-        <But Name="Samsung"></But>
-        <But Name="Samsung"></But>
-        <But Name="Samsung"></But>
-        <But Name="Samsung"></But>
-        {/* <But Name="Samsung"></But>
-        <But Name="Samsung"></But>
-        <But Name="Samsung"></But>
-        <But Name="Samsung"></But> */}
-      </Box>
-      <div className="ourPr">
-        <h2>OUR PRODUCT</h2>
+    <Box width={'100%'}>
+      <div className="w-full mt-8 mb-8 flex gap-5 overflow-hidden">
+        {items.map((item, key) => (
+          <Link to={item.to} key={key} className="contents">
+            <div
+              className="w-[calc(100%/8)] h-max flex items-center 
+                            p-[10px_15px] justify-center border-[1px] border-[rgba(0,_0,_0,_0.1)]"
+            >
+              <img
+                src={item.src}
+                alt=""
+                className="object-contain w-full h-auto"
+              />
+            </div>
+          </Link>
+        ))}
+      </div>
+      <div className="ourPr mb-5">
+        <h2>Các sản phẩm mới nhất</h2>
       </div>
     </Box>
   );
 }
+
+export default memo(ChoosePr);
